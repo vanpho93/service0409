19797215722523412d76e5610a62ecdfa09e94bf
Van Pho Nguyen <vanpho02@gmail.com>
2017-11-22 18:41:17 +0700

    xong

--- src/app/words/words.component.ts
+++ src/app/words/words.component.ts
@@ -4,14 +4,27 @@ import { WordService } from './word.service';
 @Component({
     template: `
         <div>Words</div>
+        <div>
+            <input placeholder="English" />
+            <br><br>
+            <input placeholder="Vietnamese" />
+            <br><br>
+            <button>Add</button>
+        </div>
+        <div *ngFor="let word of words">
+            <h3>{{word.en}}</h3>
+            <p>{{word.vn}}</p>
+            <button>Xoa</button>
+        </div>
     `,
     selector: 'app-words',
     providers: [WordService]
 })
 
 export class WordsComponent {
+    words = [];
     constructor(private wordService: WordService) {
         this.wordService.getAllWords()
-        .then(words => console.log(words));
+        .then(words => this.words = words);
     }
 }
